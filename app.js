be5215f1ee8830dd725a2e8cf8be33195e493ce0
mbsa13102002@gmail.com <mbsaiaditya@gmail.com>
2021-06-21 00:20:09 +0530

    commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || '5000';
+const port = process.env.PORT || 3000;
 app.use(express.static("public"));
 // app.use(bodyParser.urlencoded({extend:true}));
 app.engine('html', require('ejs').renderFile);
