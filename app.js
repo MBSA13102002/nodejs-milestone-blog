a3e2f0366e1fbdcead3cb6ed3237e3661d1c0375
mbsa13102002@gmail.com <mbsaiaditya@gmail.com>
2021-06-21 00:19:29 +0530

    commit

--- app.js
+++ app.js
@@ -2,31 +2,29 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || '5000';
-const path = require('path');
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extend:true}));
+// app.use(bodyParser.urlencoded({extend:true}));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 // app.set('views', __dirname);
-app.get("/", (req, res)=>{ 
+app.get('/', (req, res)=>{ 
     res.render('index.html')
 });
 
 app.get('/post/:postslug', (req, res)=>{
     var primary_key = req.params.postslug
-    // res.sendFile(path.join(__dirname+'/templates/post.html'));
     res.render('post.html',{primary_key:primary_key})
     
 });
 
-app.get("/about", (req, res)=>{
+app.get('/about', (req, res)=>{
     res.render('about.html')
 });
 
-app.get("/contact", (req, res)=>{
+app.get('/contact', (req, res)=>{
     res.render('contact.html')
 });
-app.get("/compose", (req, res)=>{
+app.get('/compose', (req, res)=>{
     res.render('compose.html')
 });
 
