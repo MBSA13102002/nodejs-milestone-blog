07aadfccb68ca6ab55014b7cd8520ccb20e0e07d
MBSA13102002 <73106667+MBSA13102002@users.noreply.github.com>
2021-06-21 00:14:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || '5000';
+const port = process.env.PORT || 3000;
 const path = require('path');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extend:true}));
